ec96a879fd0adc7170a4f4a8cb0fd1e314013532
Mo'men Abdulrahman <161628358+R00t105@users.noreply.github.com>
2025-09-08 14:27:36 +0300

    Delete 12-EmployeeProject directory

--- 12-EmployeeProject/src/main/java/com/java/employeeproject/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.employeeproject.Repository;
-
-import com.java.employeeproject.Model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
